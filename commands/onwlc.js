dc807e6e5a6da328d1beed457fc6f433fd6f0a56
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-01-12 12:59:33 +0700

    Update onwlc.js

--- commands/onwlc.js
+++ commands/onwlc.js
@@ -1,1 +1,13 @@
+module.exports = {
+  name: 'onwlc',
+  description: 'Bật tính năng chào mừng người dùng mới',
+  async execute(message, args) {
+    if (!args[0]) return message.reply('Vui lòng cung cấp kênh chào mừng!');
+    const channel = message.guild.channels.cache.get(args[0]);
+
+    if (!channel) return message.reply('Kênh không hợp lệ!');
+    // Lưu kênh vào database hoặc file cấu hình
+    // Cấu hình để bật tính năng chào mừng trong server
+  }
+};
 
