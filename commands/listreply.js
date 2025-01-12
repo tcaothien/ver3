30cef85457841e9fa08e8cbbdb4b563b6705ec4b
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-01-12 20:51:08 +0700

    Update listreply.js

--- commands/listreply.js
+++ commands/listreply.js
@@ -1,15 +1,23 @@
 const { MessageEmbed } = require('discord.js');
+
 module.exports = {
   name: 'listreply',
-  description: 'Liệt kê tất cả tin nhắc phản hồi',
+  description: 'Liệt kê tất cả phản hồi tự động.',
   async execute(message) {
-    const replies = await getRepliesFromDB(); // Tải danh sách phản hồi từ database
+    // Lấy tất cả phản hồi từ cơ sở dữ liệu
+    const replies = await getAllReplies(message.guild.id);
+    if (!replies || replies.length === 0) {
+      return message.reply('Không có phản hồi tự động nào được tạo.');
+    }
+
     const embed = new MessageEmbed()
       .setColor('#FF0000')
-      .setTitle('Danh sách phản hồi tự động')
-      .setDescription(replies.join('\n'));
+      .setTitle('Danh sách phản hồi tự động');
+
+    replies.forEach(reply => {
+      embed.addField(reply.keyword, reply.response);
+    });
 
     message.channel.send({ embeds: [embed] });
   }
 };
-
