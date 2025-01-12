ffe1367206b6cdbb97419ae21cbc1762b6072019
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-01-12 20:51:42 +0700

    Update dnt.js

--- commands/dnt.js
+++ commands/dnt.js
@@ -1,15 +1,19 @@
 module.exports = {
   name: 'dnt',
-  description: 'Hiển thị điểm của người dùng',
+  description: 'Hiển thị điểm của người dùng.',
   async execute(message) {
-    const user = message.author;
-    const userData = await User.findOne({ userId: user.id });
+    const userId = message.author.id;
+    const user = await User.findOne({ userId });
 
-    if (!userData) {
+    if (!user) {
       return message.reply('Bạn chưa có điểm nào!');
     }
 
-    message.reply(`Điểm của bạn là: ${userData.points}`);
+    const embed = new MessageEmbed()
+      .setColor('#00FF00')
+      .setTitle(`Điểm của ${message.author.tag}`)
+      .setDescription(`Điểm của bạn: ${user.points}`);
+
+    message.channel.send({ embeds: [embed] });
   }
 };
-
