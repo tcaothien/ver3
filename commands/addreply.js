56cd6a0705693f6849302fc742b4f88ee3774f91
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-01-12 13:00:13 +0700

    Update addreply.js

--- commands/addreply.js
+++ commands/addreply.js
@@ -1,1 +1,10 @@
+module.exports = {
+  name: 'addreply',
+  description: 'Thêm tin nhắc phản hồi tự động',
+  async execute(message, args) {
+    const replyMessage = args.join(' ');
+    // Lưu tin nhắc vào cơ sở dữ liệu hoặc cấu hình bot
+    message.reply(`Đã thêm phản hồi: "${replyMessage}"`);
+  }
+};
 
