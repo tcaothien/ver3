8ba92408b47b7b59462dc64821106f8bdbe92342
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-01-12 21:05:38 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  token: 'YOUR_BOT_TOKEN', // Thay YOUR_BOT_TOKEN bằng token của bot Discord
+  mongoURI: 'YOUR_MONGODB_URI', // Thay YOUR_MONGODB_URI bằng URL kết nối MongoDB của bạn
+  prefix: 'e', // Tiền tố lệnh của bot
+  welcomeChannel: 'YOUR_WELCOME_CHANNEL_ID', // Thay YOUR_WELCOME_CHANNEL_ID bằng ID kênh chào mừng
+};
