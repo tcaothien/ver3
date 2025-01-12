02c86a98d617a5e613731c3a89cf10b9bfb83cac
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-01-12 13:01:31 +0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  userId: String,
+  lastUsed: Number, // Thời gian sử dụng lệnh tarot gần nhất
+  points: { type: Number, default: 0 }, // Điểm người dùng
+});
+
+module.exports = mongoose.model('User', userSchema);
 
