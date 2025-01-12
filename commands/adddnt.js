c3ba601c607524a9a98978a2fe70ea51c7dfc1ec
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-01-12 20:52:25 +0700

    Update adddnt.js

--- commands/adddnt.js
+++ commands/adddnt.js
@@ -1,27 +1,27 @@
 module.exports = {
   name: 'adddnt',
-  description: 'Cộng điểm donate cho người donate',
+  description: 'Cộng điểm donate cho người dùng.',
   async execute(message, args) {
+    // Kiểm tra quyền của người dùng
     if (!message.member.roles.cache.some(role => ['1291261923001765948', '1313801586916462613', '1285915798308651021'].includes(role.id))) {
       return message.reply('Bạn không có quyền sử dụng lệnh này!');
     }
 
-    const targetUser = message.mentions.users.first();
-    if (!targetUser) return message.reply('Vui lòng chỉ định người dùng.');
+    const userId = args[0];
+    const points = parseInt(args[1]);
 
-    const pointsToAdd = parseInt(args[1], 10);
-    if (isNaN(pointsToAdd)) return message.reply('Điểm cộng phải là một số hợp lệ.');
-
-    const userData = await User.findOne({ userId: targetUser.id });
-    if (!userData) {
-      return message.reply('Người dùng này chưa tồn tại trong hệ thống.');
+    if (!userId || isNaN(points)) {
+      return message.reply('Vui lòng cung cấp ID người dùng và số điểm hợp lệ!');
     }
 
-    userData.points += pointsToAdd;
-    await userData.save();
+    const user = await User.findOne({ userId });
+    if (!user) {
+      return message.reply('Người dùng này không tồn tại!');
+    }
 
-    message.reply(`Đã cộng ${pointsToAdd} điểm cho ${targetUser.username}.`);
+    user.points += points;
+    await user.save();
+
+    message.reply(`Đã cộng ${points} điểm cho ${userId}.`);
   }
 };
-
-
