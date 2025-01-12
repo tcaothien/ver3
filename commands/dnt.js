23aaaa502957fa375ac2370830016791702e328b
tcaothien <116654835+tcaothien@users.noreply.github.com>
2025-01-12 13:02:45 +0700

    Update dnt.js

--- commands/dnt.js
+++ commands/dnt.js
@@ -1,1 +1,15 @@
+module.exports = {
+  name: 'dnt',
+  description: 'Hiển thị điểm của người dùng',
+  async execute(message) {
+    const user = message.author;
+    const userData = await User.findOne({ userId: user.id });
+
+    if (!userData) {
+      return message.reply('Bạn chưa có điểm nào!');
+    }
+
+    message.reply(`Điểm của bạn là: ${userData.points}`);
+  }
+};
 
